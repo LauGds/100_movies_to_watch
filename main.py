b3bcd301ed4ff3d3012251d369925efe78431750
LauGds <137927575+LauGds@users.noreply.github.com>
2023-08-17 09:54:34 -0700

    main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-
-
 response = requests.get(URL)
 empire_page = response.content
 
