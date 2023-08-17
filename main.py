cac98219e18640600c3785fbd65196c70dd5f438
LauGds <137927575+LauGds@users.noreply.github.com>
2023-08-17 09:53:54 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+
+response = requests.get(URL)
+empire_page = response.content
+
+soup = BeautifulSoup(empire_page, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+titles_list = [title.getText() for title in all_movies]
+movies = titles_list[::-1]
+
+with open("movies.txt", mode="w", encoding="utf8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
